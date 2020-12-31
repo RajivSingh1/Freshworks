df7b7b05c6ff70bc582ccdc0d2c15450f60d380a
Rajiv Singh <rs481441@gmail.com>
2020-12-31 17:17:51 +0530

    adding maven dependency for JSON

---
+++ src/main/java/StudentDataStore.java
@@ -0,0 +1,13 @@
+import org.json.simple.JSONObject;
+
+public class StudentDataStore {
+    Detail detail;
+
+    public StudentDataStore(Detail detail) {
+        this.detail = detail;
+    }
+    public void insert()
+    {
+        JSONObject json = new JSONObject();
+    }
+}
